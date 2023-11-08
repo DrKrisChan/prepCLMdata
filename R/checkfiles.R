20a2028cc8e7f6dd0293dde8f7a72c8b64a64353
Dr Kristofer Chan <58226561+DrKrisChan@users.noreply.github.com>
2023-11-08 11:36:36 +0000

    Add files via upload

--- R/checkfiles.R
+++ R/checkfiles.R
@@ -1,13 +1,19 @@
 checkfiles<-function(){
-  prec=list.files(, pattern="prec", recursive = T)
-  tmin=list.files(, pattern="tmin", recursive = T)
-  tmax=list.files(, pattern="tmax", recursive = T)
-  grd=list.files(, pattern=".grd", recursive = T)
+  precmultiple=FALSE
+  tminmultiple=FALSE
+  tmaxmultiple=FALSE
+  prec=list.files(, pattern="prec", recursive = T) #or can specify dir, e.g. filenames=list.files('D:/batdata/test', recursive = T)
+  tmin=list.files(, pattern="tmin", recursive = T) #or can specify dir, e.g. filenames=list.files('D:/batdata/test', recursive = T)
+  tmax=list.files(, pattern="tmax", recursive = T) #or can specify dir, e.g. filenames=list.files('D:/batdata/test', recursive = T)
+  grd=list.files(, pattern=".grd", recursive = T) #or can specify dir, e.g. filenames=list.files('D:/batdata/test', recursive = T)
   if(length(grd)!=0){warning(paste0(".grd files already found in ",getwd()," Please ensure you are happy to progress as files will be overwritten"))}
-  if(length(prec)==0){warning(paste0("no rain file in ",getwd()))}
+  if(length(prec)==0){warning(paste("no rain file in ",getwd()))}
+  if((length(prec)>1)&(length(prec)==12)){warning(paste("12 precipitation files found in ",getwd(), ". Assuming individual bands for each month."))}else if((length(prec)>1)&(length(prec)!=12)){warning(paste("multiple precipitation files found in ",getwd(),". Please ensure only one precip file is used in folder (unless individual monthly files are used)."))}
   else{print(paste0("rain file found: ",prec))}
-  if(length(tmin)==0){warning(paste0("no tmin file in ",getwd()))}
+  if(length(tmin)==0){warning(paste("no tmin file in ",getwd()))}
+  if((length(tmin)>1)&(length(tmin)==12)){warning(paste("12 tmin files found in ",getwd(), ". Assuming individual bands for each month."))}else if((length(tmin)>1)&(length(tmin)!=12)){warning(paste("multiple tmin files found in ",getwd(),". Please ensure only one tmin file is used in folder (unless individual monthly files are used)."))}
   else{print(paste0("tmin file found: ",tmin))}
-  if(length(tmax)==0){warning(paste0("no tmax file in ",getwd()))}
+  if(length(tmax)==0){warning(paste("no tmax file in ",getwd()))}
+  if((length(tmax)>1)&(length(tmax)==12)){warning(paste("12 tmax files found in ",getwd(), ". Assuming individual bands for each month."))}else if((length(tmax)>1)&(length(tmax)!=12)){warning(paste("multiple tmax files found in ",getwd(),". Please ensure only one tmax file is used in folder (unless individual monthly files are used)."))}
   else{print(paste0("tmax file found: ",tmax))}
 }
--- R/prepclmdata.R
+++ R/prepclmdata.R
@@ -1,19 +1,32 @@
 prepclmdata <- function(){
+  x=1
   prec=list.files(, pattern="prec", recursive = T) #or can specify dir, e.g. filenames=list.files('D:/batdata/test', recursive = T)
   tmin=list.files(, pattern="tmin", recursive = T) #or can specify dir, e.g. filenames=list.files('D:/batdata/test', recursive = T)
   tmax=list.files(, pattern="tmax", recursive = T) #or can specify dir, e.g. filenames=list.files('D:/batdata/test', recursive = T)
   for (x in 1:12){
-    print(paste("resolving band ", x, " of 12", sep=""))
-    img <- raster(prec, band=x)
-    writeRaster(img, paste(getwd(),"/rain",x,".grd",sep=""), overwrite=TRUE)
-    img <- raster(tmin, band=x)
-    img = img*10
-    writeRaster(img, paste(getwd(),"/tmin",x,".grd",sep=""), overwrite=TRUE)
-    img <- raster(tmax,band = x)
-    img = img*10
-    writeRaster(img, paste(getwd(),"/tmax",x,".grd",sep=""), overwrite=TRUE)
+    if((length(prec)==12)&(length(tmin)==12)&(length(tmax)==12)){
+      print(paste("resolving band ", x, " of 12", sep=""))
+      img <- raster(prec[x])
+      writeRaster(img, paste(getwd(),"/rain",x,".grd",sep=""), overwrite=TRUE)
+      img <- raster(tmin[x])
+      img = img*10
+      writeRaster(img, paste(getwd(),"/tmin",x,".grd",sep=""), overwrite=TRUE)
+      img <- raster(tmax[x])
+      img = img*10
+      writeRaster(img, paste(getwd(),"/tmax",x,".grd",sep=""), overwrite=TRUE)
+      img <- raster(tmax[1])
+    } else{
+      print(paste("resolving band ", x, " of 12", sep=""))
+      img <- raster(prec, band=x)
+      writeRaster(img, paste(getwd(),"/rain",x,".grd",sep=""), overwrite=TRUE)
+      img <- raster(tmin, band=x)
+      img = img*10
+      writeRaster(img, paste(getwd(),"/tmin",x,".grd",sep=""), overwrite=TRUE)
+      img <- raster(tmax,band = x)
+      img = img*10
+      writeRaster(img, paste(getwd(),"/tmax",x,".grd",sep=""), overwrite=TRUE)
+      img <- raster(tmax,band = 1)}
   }
-  img <- raster(tmax,band = 1)
   img[img>-10000] <- 1
   writeRaster(img, paste(getwd(),"/alt.grd",sep=""), overwrite=TRUE)
   return(paste0("Files successfully prepared! Directory for reference: ",getwd()))
